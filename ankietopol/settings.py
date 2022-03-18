3b24fdc45b6756d115e5ef7dcaea91820bc9d81f
mateusz machura <machurka123@gmail.com>
2022-03-18 09:10:49 +0100

    home page + example

--- ankietopol/settings.py
+++ ankietopol/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['ankietopol.herokuapp.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'ankiety.apps.AnkietyConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
