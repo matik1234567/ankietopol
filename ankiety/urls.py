31092c7a14fceb8bf940227d0d2c95a1d48cfee6
michalmoric <56163212+michalmoric@users.noreply.github.com>
2022-03-27 14:38:52 +0200

    Dodano strone logowania i wylogowywanie

--- ankiety/urls.py
+++ ankiety/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('', views.home),
+    path('login/',views.CustomLoginView.as_view(),name='login'),
+    path('logout/',LogoutView.as_view(next_page='home'),name='logout'),
+    path('', views.home,name='home'),
     path('test', views.test)  # dev purpose
 ]
--- ankiety/views.py
+++ ankiety/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from ankiety.tests import TestMainDB
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
 # Create your views here.
+class CustomLoginView(LoginView):
+    template_name = 'ankiety/login.html'
+    fields = '_all_'
+    redirect_authenticated_user = True
 
+    def get_success_url(self):
+        return reverse_lazy('home')
 # example
 data = [
     {
