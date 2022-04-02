0b480462b0b5ecb33e22d0a73a97fe9b4d29069a
Gacus <haniv1001@gmail.com>
2022-04-02 22:02:16 +0200

    created poll view

--- ankiety/urls.py
+++ ankiety/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
     path('', views.home, name='home'),
-    path('kreator-ankiety', views.create_poll, name='create_poll'),
+    path('kreator-ankiety/', views.create_poll, name='create_poll'),
+    path('ankieta/', views.poll, name='poll'),
     path('test', views.test),  # dev purpose
     path('test-form/', views.test_form, name='tf'),  # dev purpose
 ]
--- ankiety/views.py
+++ ankiety/views.py
@@ -24,6 +24,54 @@ data = [
     }
 ]
 
+formItems = [
+    {
+      "id": 0,
+      "type": "c",
+      "description": "headline of item 1",
+      "value": ["description of checkbox 1", "description of checkbox 2"],
+      "name": "shshhs",
+      "placeholder": "",
+      "is_req": "T"
+    },
+    {
+      "id": 1,
+      "type": "t",
+      "description": "headline of item 2",
+      "value": "placeholder(optionally)",
+      "name": "shshhs",
+      "placeholder": "",
+      "is_req": "T"
+    },
+    {
+      "id": 2,
+      "type": "r",
+      "description": "headline of item 3",
+      "value": ["description of radio 1", "description of radio 2"],
+      "name": "shshhs",
+      "placeholder": "",
+      "is_req": "F"
+    },
+    {
+      "id": 3,
+      "type": "s",
+      "description": "headline of item 4",
+      "value": [0, 5],
+      "name": "shshhs",
+      "placeholder": "",
+      "is_req": "T"
+    },
+    {
+      "id": 4,
+      "type": "n",
+      "description": "headline of item 4",
+      "value": [5, 80],
+      "name": "shshhs",
+      "placeholder": "",
+      "is_req": "T"
+    }
+  ]
+
 # return home view
 def home(request):
     return render(request, 'ankiety/home.html', {'books': data})
@@ -32,7 +80,13 @@ def home(request):
 def create_poll(request):
     if request.method == "POST":
         DBManager.convert_to_json(request.POST, 1)  # user id
-    return render(request, 'ankiety/create_poll.html')
+    return render(request, 'ankiety/poll_create.html')
+
+# poll view
+def poll(request):
+    if request.method == "POST":
+        print(request.POST)
+    return render(request, 'ankiety/poll.html', {'polls': formItems})
 
 # dev purpose for database testers
 def test(request):
