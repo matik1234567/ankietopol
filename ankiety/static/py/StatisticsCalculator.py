8c68d19eacf25d53c5b365bb3c82dcd3448b63a5
Aneta <anetanowak55@gmail.com>
2022-04-24 15:53:25 +0200

    update

--- ankiety/static/py/StatisticsCalculator.py
+++ ankiety/static/py/StatisticsCalculator.py
@@ -92,21 +92,56 @@ class StatisticsCalculator:
                 count += 1
         return count'''
 
-    @staticmethod
+    ''''@staticmethod
     def __merge_lists(lists):
         result = []
         for list in lists:
+<<<<<<< Updated upstream
             if list is list:
                 result.extend(list)
             else:
                 result.append(list)
+=======
+            result.extend(list)
+        return result'''
+
+    @staticmethod
+    def __expand_lists_in_df(df):
+        result = []
+        for list in lists:
+            result.extend(list)
+>>>>>>> Stashed changes
         return result
 
     @staticmethod
     def get_correlation(poll, responses, var1_id, var2_id):
         var1_vals = responses[var1_id]
         var2_vals = responses[var2_id]
+
         # TODO - check what kind of variables, calculate correlation
+        if StatisticsCalculator.__get_variable_type(poll["type"][var1_id]) == StatisticsCalculator.__get_variable_type(poll["type"][var2_id]):
+            nothing = 0
+            # variable types are the same
+            description = "method used: ..., if index>1 corelation exists ..."
+            index = 0.5
+        else:
+            nothing = 0
+            # there is one categorical and one continuous variable
+            description = "method used: ..., if index>1 corelation exists ..."
+            index = 0.5
+        return description, index
+
+    @staticmethod
+    def __get_variable_type(field_type):
+        match field_type:
+            case 'r':
+                return "categorical"
+            case 'c':
+                return "categorical"
+            case 'n':
+                return "continuous"
+            case 's':
+                return "continuous"
 
 """
 df_r = pd.read_json("C:\\Users\\aneta\\Documents\\GitHub\\ankietopol\\ankietopol\\ankiety\\static\\examples\\responses2.json")
@@ -114,4 +149,5 @@ print(df_r)
 df_p = pd.read_json("C:\\Users\\aneta\\Documents\\GitHub\\ankietopol\\ankietopol\\ankiety\\static\\examples\\items2.json")
 print(df_p)
 print(StatisticsCalculator.get_basic_measurements(df_p, df_r))
+print(get_correlation(df_p, df_r, 0, 2))
 """
